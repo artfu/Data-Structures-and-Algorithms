59a0d7f8bc63876d1bbeba2074e9cb5be8a9a919
artfu <fuxin92@gmail.com>
2021-07-21 01:26:31 +0800

    rewrite double linked list

---
+++ C/Data-Structures/LinkedList/2-double_linked_list.c
@@ -0,0 +1,28 @@
+/**
+ * @file        2-linked_list.c 
+ * @author  
+ * @date    
+ * 
+ * @brief       Implement double linked lists(NO header) and related operations, 
+ *              including insert, search, delete and other operations. 
+ * 
+ * @details     1. head_insert() -- Insert a node at the Head of list
+ *              2. tail_insert() -- Insert a node at the END of list
+ *              3. insert() -- Insert a node at the specific position of list
+ *
+ *              4. pos_of() -- search the position of specific value
+ *              5. value_of() -- find the value on specific position 
+ *              6. head_loc() -- locate the first node of list
+ *              7. tail_loc() -- locate the last node of list
+ *              8. prev() -- locate the previous node
+ *              9. advan() -- locate the advanced node
+ *
+ *              10. del_node() -- delete node through the data
+ *              11. del_list() -- delete the whole list
+ *
+ *              12. is_empty() -- check if the list is empty
+ *              13. is_last() -- check if the node is the last one
+ *
+ *              14. print() -- print the list
+ */
+
