2dec5ce673b641f1cfb317a41beb71e9c28d3015
artfu <fuxin92@gmail.com>
2021-07-27 21:31:07 +0800

    complete 3-circular_linked_list.c

--- C/Data-Structures/LinkedList/3-circular_linked_list.c
+++ C/Data-Structures/LinkedList/3-circular_linked_list.c
@@ -5,9 +5,10 @@
  * 
  * @brief       Implement circular linked lists and related operations, 
  * 
- * @details     What's the biggest difference? if plast is the pointer to last node:
- *              The last node of singly linked list: plast->next = NULL;
- *              The last node of circular linked list: plast->next = head;(the pointer to list head node)
+ * @details     What's the biggest difference between singly liked list and circular linked list? 
+ *              Head node pointer: head;        Last node pointer: tail
+ *              singly linked list      -- tail->next = NULL;
+ *              circular linked list    -- tail->next = head;
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +18,40 @@ struct node {
         struct node *next;
 };
 
-/* remove func -- we'll talk about Linus Torvalds good taste linked list */
+/****************************     CREATE     **********************************/
+/**
+ * @brief       create -- creat a list from empty
+ * 
+ * @details     
+ * 
+ * @note        Let's do the third way -- Use pointer to node as return value
+ * 
+ * @param       head_ref        -- pointer to pointer                 
+ * @param       data            -- int value
+ * 
+ * @return      void
+ * @retval      void
+*/
+struct node* create(struct node *head)
+{
+        struct node *ptem = (struct node*) malloc(sizeof(struct node));
+        int data;
+        printf("Input an int value: ");
+        scanf("%d", &data);
+        ptem->data = data;
+
+        if (head == NULL) 
+                /* the special part I: */
+                ptem->next = ptem;
+
+        ptem->next = head;
+        head = ptem;
+
+
+        return head;
+}
+
+/* remove -- we'll talk about Linus Torvalds good taste linked list */
 void bad_remove(struct node **head_ref, struct node *target)
 {
         struct node *curr = *head_ref;
@@ -35,7 +69,7 @@ void bad_remove(struct node **head_ref, struct node *target)
 
 void elegant_remove(struct node **head_ref, struct node *target)
 {
-        struct node **ptr = **head_ref;
+        struct node **ptr = &(*head_ref);
 
         while ((*ptr) != target)
                 ptr = &(*ptr)->next;
@@ -45,5 +79,17 @@ void elegant_remove(struct node **head_ref, struct node *target)
 
 int main(int argc, char** argv)
 {
+        struct node *head = NULL;
+        head = create(head);
+        head = create(head);
+        head = create(head);
+
+        struct node *ptr = head;
+        /* the special part II: */
+        while (ptr->next = head) {
+                printf("%d ", ptr->data);
+                ptr = ptr->next;
+        }
+
         return 0;
 }
