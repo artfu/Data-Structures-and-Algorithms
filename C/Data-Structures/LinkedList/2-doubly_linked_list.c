d111a665b6cf30907377e2322ce3c16d2b386e45
artfu <fuxin92@gmail.com>
2021-07-27 19:57:04 +0800

    add delete() func

--- C/Data-Structures/LinkedList/2-doubly_linked_list.c
+++ C/Data-Structures/LinkedList/2-doubly_linked_list.c
@@ -4,9 +4,9 @@
  * @date        2020.7.21 
  * 
  * @brief       Implement double linked lists(NO header) and related operations, 
- *              including insert, search, delete and other operations. 
+ *              including insert and delete operations. 
  * 
- * @details     
+ * @details     Other function, like search, print are same to singly linked list.
  */
 
 #include <stdio.h>
@@ -21,7 +21,7 @@ struct node {
 /****************************     INSERT     **********************************/
 
 /**
- * @brief       insert() -- insert a node to the head or tail of a list      
+ * @brief       insert -- insert a node to the head or tail of a list      
  * 
  * @details     like singly linked list, there are 3 conditons:
  *              1. add next node to head;
@@ -59,10 +59,10 @@ void head_insert(struct node **head_ref, int data)
 
 void tail_insert(struct node **head_ref, int data) 
 { 
+        struct node *tail = *head_ref;
         struct node *ptr = (struct node*) malloc(sizeof(struct node));
         ptr->data = data;
 
-        struct node *tail = *head_ref;
         if (tail != NULL) {
                 while (tail->next != NULL)
                         tail = tail->next;
@@ -76,6 +76,84 @@ void tail_insert(struct node **head_ref, int data)
                 *head_ref = ptr;
         }
 } 
+/* insert node to the i-th node */
+void i_insert(struct node **head_ref, int data, int i)
+{
+        struct node *ptem = (struct node*) malloc(sizeof(struct node));
+        ptem->data = data;
+
+        struct node *ptr = *head_ref;
+        int j = 1;
+        
+        while (p != NULL && j < i) {
+                ptr = ptr->next;
+                j++;
+        }
+
+        ptr->prev->next = ptem;
+        ptem->prev = ptr->prev;
+        ptem->next = ptr;
+        ptr->prev = ptem;
+}
+
+/* README: COMMENT OUT TO RUN */
+void p_insert(struct node **head_ref, int data, struct node* pos)
+{
+        struct node *ptem = (struct node*) malloc(sizeof(struct node));
+        ptem->data = data;
+
+        /* insert node after pos */
+        pos->next->prev = ptem;
+        ptem->next = pos->next;
+        pos->next->ptem;
+        ptem->prev = pos;
+
+        /* insert node before pos */
+        pos->prev->next = ptem;
+        ptem->prev = pos->prev;
+        ptem->next = pos;
+        pos->prev = ptem;
+}
+
+/****************************     DELETE     **********************************/
+
+/**
+ * @brief       delete() -- delete a node from list
+ * 
+ * @details     
+ *              
+ * @note        
+ * 
+ * @param       head_ref        -- pointer to pointer                 
+ * @param       i               -- index
+ * 
+ * @return      void
+ * @retval      void
+*/
+/* delete the i-th node (i>0) */
+void i_delete(struct node **head_ref, int i)
+{
+        struct node *ptem = *head_ref;
+        int j = 1;
+
+        while (j < i && ptem != NULL) {
+                ptem = ptem->next;
+                j++;
+        }
+
+        ptem->next->prev = ptem->prev;
+        ptem->prev->next = ptem->next;
+        free(ptem);
+}
+
+void p_delete(struct node **head_ref, struct node *pos)
+{
+        struct node *ptem = *head_ref;
+
+        pos->next->prev = pos->prev;
+        pos->prev->next = pos->next;
+        free(pos);
+}
 
 int main(int argc, char* argv[])
 {
