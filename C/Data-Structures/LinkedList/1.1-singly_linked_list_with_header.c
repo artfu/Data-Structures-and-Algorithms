7b90376c5f870c672285df1814ea71d06c95214b
artfu <fuxin92@gmail.com>
2021-08-05 17:24:24 +0800

    sequential_queue.c

--- C/Data-Structures/LinkedList/1.1-singly_linked_list_with_header.c
+++ C/Data-Structures/LinkedList/1.1-singly_linked_list_with_header.c
@@ -19,23 +19,46 @@ struct header {
         struct node *head;
 };
 
-struct header* init()
+struct header* init1()
 {
         struct header *list = (struct header*) malloc(sizeof(struct header));
         list->head = NULL;
         return list;
 }
 
-void add_node(struct node **head, int data)
+struct node* init2(struct node* head)
+{
+        struct node *p = (struct node*) malloc(sizeof(struct node));
+        if (!p) {
+                printf("malloc error.\n");
+        } else {
+                p->next = NULL;
+                head = p;
+        }
+        return head;
+}
+
+void add_node1(struct node **head, int data)
 {
         struct node *p = (struct node*) malloc(sizeof(struct node));
         if (!p) {
                 printf("Unable to allocate memory.\n");
-                exit(1);
         } else {
                 p->data = data;
                 p->next = *head;
                 *head = p;
+        }
+}
+
+void add_node2(struct node *head, int data)
+{
+        struct node *p = (struct node*) malloc(sizeof(struct node));
+        if (p) {
+                p->data = data;
+                p->next = head->next;
+                head->next = p;
+        } else {
+                printf("ERROR.\n");
         }
 }
 
---
+++ C/Data-Structures/Queue/1-sequential_queue.c
@@ -0,0 +1,56 @@
+/**
+ * @file        1-sequential_queue.c
+ * @author      artfu
+ * @date        2020.7.27
+ *
+ * @brief       circular queue
+ *
+ * @details
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXSIZE 10
+
+struct queue {
+        int *base;
+        int front, rear;
+};
+
+void init(struct queue *q)
+{
+        q = (struct queue*) malloc(sizeof(struct q));
+        if (q) {
+                q->base = (int*) malloc(MAXSIZE * sizeof(int));
+                q->front = q->rear = 0;
+        } else {
+                printf("Error.\n");
+        }
+}
+
+void enqueue(struct queue *q, int data)
+{
+        if((q->rear + 1) % MAXSIZE == q->front)
+                printf("The queue is FULL.\n");
+
+        *(q->base + q->rear) = data;
+        q->rear = (q->rear + 1) % MAXSIZE; 
+}
+
+void dequeue(struct queue *q)
+{
+        if (q->rear == q->front)
+                printf("The queue is EMPTY.\n");
+
+        q->front = (q->front + 1) % MAXSIZE;
+}
+
+int length(struct queue *q)
+{
+        return (q->rear - q->front + MAXSIZE) % MAXSIZE;
+}
+
+int main(int argc, char** argv)
+{
+        return 0;
+}
